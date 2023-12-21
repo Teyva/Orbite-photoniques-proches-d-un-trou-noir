db4ef334383a2411fe6150b9bc15dc0f8010f87d
Teyva <154484372+Teyva@users.noreply.github.com>
2023-12-20 21:39:03 -0500

    Update BoncodepourRisco.m

--- BoncodepourRisco.m
+++ BoncodepourRisco.m
@@ -1,13 +1,11 @@
-M = 1; % Valeur fixe de M
-alpha_values = linspace(-8, 1, 100); % Créez un vecteur d'alpha
-r_solutions = zeros(size(alpha_values)); % Initialisez un tableau pour les solutions r
+M = 1; 
+alpha_values = linspace(-8, 1, 100); 
+r_solutions = zeros(size(alpha_values)); 
 
 for i = 1:length(alpha_values)
     alpha = alpha_values(i);
     equation_to_solve = @(r) fonctionisco(r, alpha, M) - r;
-    
-    % Fournissez une estimation initiale pour r
-    % Assurez-vous que l'estimation initiale n'est pas égale à zéro
+
     r_initial_guess = 7; % Choisissez une valeur non nulle
 
     try
